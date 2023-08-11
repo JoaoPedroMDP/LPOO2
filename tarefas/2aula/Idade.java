e4966d7448baec5404738d767d63236115cdccee
Camila <camila.verssao@gmail.com>
2023-08-11 11:14:50 -0300

    [add] Classe Idade criada

---
+++ tarefas/2aula/Idade.java
@@ -0,0 +1,21 @@
+public class Idade {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Albert Einstein", 14, 3, 1879);
+        Pessoa p2 = new Pessoa("Isaac Newton", 4, 1, 1642);
+
+        CalculaIdadeFunction calculaIdadeFunction = (Pessoa pessoa) -> {
+            pessoa.calculaIdade();  
+            return pessoa.informaIdade();
+        };
+        
+        System.out.println(p1.informaNome() + " teria " + calculaIdadeFunction.calculaIdade(p1) + " anos hoje.");
+        System.out.println(p2.informaNome() + " teria " + calculaIdadeFunction.calculaIdade(p2) + " anos hoje.");
+    }
+}
+
+interface CalculaIdadeFunction {
+    Integer calculaIdade(Pessoa pessoa);
+}
+
+
+
